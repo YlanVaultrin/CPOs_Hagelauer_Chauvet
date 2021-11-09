adad8946b93eab3e217a71c89d7c075ff65f4a3a
ylanc <ylanc@LAPTOP-FNG0NHGG>
2021-11-09 22:22:27 +0100

    classe

---
+++ src/sp4_hagelauer_chauvet/Partie.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_hagelauer_chauvet;
+
+/**
+ *
+ * @author ylanc
+ */
+public class Partie {
+    
+}
