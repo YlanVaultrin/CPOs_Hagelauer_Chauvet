5904ac25b143b1e7b09a64df7df5f6df768b5a4f
ylanc <ylanc@LAPTOP-FNG0NHGG>
2021-11-10 09:12:54 +0100

    début partie

--- src/sp4_hagelauer_chauvet/Partie.java
+++ src/sp4_hagelauer_chauvet/Partie.java
@@ -9,5 +9,22 @@ package sp4_hagelauer_chauvet;
  * @author ylanc
  */
 public class Partie {
-    
+   Joueur ListeJoueurs[]= new Joueur[2];//création d'un objet Joueur
+   Joueur joueurCourant;//création d'un objet Joueur
+   Grille grilleJeu= new Grille();//création d'un objet Grille
+   
+   void attribuerCouleursAuxJoueurs(){ //Associe une couleur aux 2 joueurs stocké dans Couleurs
+       double a= Math.random();//création d'une variable aléatoire entre 0 et 1
+       if (a==1){
+           ListeJoueurs[0].Couleur= "Rouge";
+           ListeJoueurs[1].Couleur= "Jaune";
+       }
+       if (a==0){
+           ListeJoueurs[1].Couleur= "Rouge";
+           ListeJoueurs[0].Couleur= "Jaune";
+       }
+   }
+   void initialiserPartie(){
+       //grilleJeu.viderGrille();
+   }
 }
