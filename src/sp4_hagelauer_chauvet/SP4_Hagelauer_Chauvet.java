f540c10d175f00f88b8ad6fca8aa7f2028d3917a
ylanc <ylanc@LAPTOP-FNG0NHGG>
2021-11-17 18:54:54 +0100

    en progrès

--- src/sp4_hagelauer_chauvet/SP4_Hagelauer_Chauvet.java
+++ src/sp4_hagelauer_chauvet/SP4_Hagelauer_Chauvet.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package sp4_hagelauer_chauvet;
+import java.util.Scanner;
 
 /**
  *
@@ -14,10 +15,13 @@ public class SP4_Hagelauer_Chauvet {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Grille grilletest = new Grille();
-        Jeton jet = new Jeton("Rouge");
-        grilletest.ajouterJetonDansColonne(jet, 3);
-        grilletest.afficherGrilleSurConsole();
+                System.out.println("Veuillez démarrer la partie!");
+                Joueur joueur1= new Joueur("nom");
+                //nextLine
+                Joueur joueur2= new Joueur("nom");
+                Partie partie= new Partie(joueur1,joueur2);
+                partie.initialiserPartie();
+                partie.debuterPartie();
     }
     
 }
